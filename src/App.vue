3934791622f5b33243555f88c5acf1e21429538e
manuelarevalo01 <manferare1@gmail.com>
2021-08-21 08:23:06 -0500

    :construction_worker: project finish platzy Vue.js 2

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <main>
+    <px-header :links="links" />
+
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+  </main>
+</template>
+
+<script>
+import PxHeader from '@/components/PxHeader'
+
+export default {
+  name: 'app',
+  components: { PxHeader },
+
+  data() {
+    return {
+      links: [
+        {
+          title: 'BTC',
+          to: { name: 'coin-detail', params: { id: 'bitcoin' } }
+        },
+        {
+          title: 'ETH',
+          to: { name: 'coin-detail', params: { id: 'ethereum' } }
+        },
+        {
+          title: 'XRP',
+          to: { name: 'coin-detail', params: { id: 'ripple' } }
+        },
+        {
+          title: 'ADA',
+          to: { name: 'coin-detail', params: { id: 'cardano' } }
+        },
+        {
+          title: 'BNB',
+          to: { name: 'coin-detail', params: { id: 'binance-coin' } }
+        },
+        {
+          title: 'USDT',
+          to: { name: 'coin-detail', params: { id: 'tether' } }
+        },
+        {
+          title: 'DOGE',
+          to: { name: 'coin-detail', params: { id: 'dogecoin' } }
+        },
+        {
+          title: 'BCH',
+          to: { name: 'coin-detail', params: { id: 'bitcoin-cash' } }
+        },
+        {
+          title: 'ICP',
+          to: { name: 'coin-detail', params: { id: 'litecoin' } }
+        },
+        {
+          title: 'LTC',
+          to: { name: 'coin-detail', params: { id: 'stellar' } }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
